c44789a2fcd6dbaf975e56e614a50c0b7297cb31
Rohit Truesdale <rohit.trues@gmail.com>
2023-06-16 17:18:53 -0500

    lab1

---
+++ labs/lab1/COSC_2325_Lab_1_RohitTruesdale.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+void main ()
+{
+    const unsigned char c1 (0x11);
+    const unsigned char c2 (0x22);
+    const unsigned char c3 (0x33);
+    const unsigned char c4 (0x44);
+    unsigned long i1 (0);
+    _asm {      
+        mov AH, c1       
+        mov AL, c2       
+        sh1 EAX, 16
+        mov AH, c3
+        mov Al, c4
+        mov EAX, i1
+    }   
+
+        
+cout << "result is " << hex << i1 << endl;
+}
